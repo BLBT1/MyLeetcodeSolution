425861ca569063f66d7b001384649c41c61ed6a6
anqiwa <andywang19991118@gmail.com>
2023-01-07 23:49:14 -0800

    149

--- Greedy/134. Gas Station/134. Gas Station.cpp
+++ Greedy/134. Gas Station/134. Gas Station.cpp
@@ -1,29 +1,38 @@
-class Solution
-{
+class Solution {
 public:
-    int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
     {
-        int total = 0;
-        for (int i = 0; i < cost.size(); ++i)
-            total += gas[i] - cost[i];
-        if (total < 0)
-            return -1;
-
-        int leftover = 0;
-        int res = 0;
-        for (int i = 0; i < cost.size(); ++i)
+        const int n = cost.size();
+        int sum = 0;
+        // 如果gas的总和 小于 cost的总和，那肯定不行
+        for(int i = 0; i < n; i++)
         {
-            leftover += gas[i] - cost[i];
-
-            // assume C is the first pt that can not be reached from res
-            // then we cannot finish in any pt in [res:C]
-            if (leftover < 0)
+            sum += gas[i] - cost[i];
+        }
+        if(sum < 0)
+            return -1;
+        
+        // 如果gas的总和 大于 cost的总和，那肯定有解。且题目告知我们只有一个解，
+        // 所以那个解肯定是存在的，只需要 循环一边所有的点找到他就可以
+        
+        // A -> B 走不下去了，说明 在AB 线路上任意一点都走不到B，假设这点是c
+        // A -> C -> B
+        // 因为 A能走到C， 所以 A->C 个 gas  > A->C 的cost
+        // 那 A->B 的 gas - A->B 的cost > C->B 的gas - C->B的cost
+        // 我们下一个起点只能是 B+1 的位置开始找
+        sum = 0;
+        int curr = 0;
+        for(int i = 0; i < n; ++i)
+        {
+            sum += gas[i] - cost[i];
+            if(sum < 0)
             {
-                res = i + 1;
-                leftover = 0;
+                sum = 0;
+                curr = i+1;
             }
         }
-
-        return res;
+        
+        return curr;
     }
 };
+
--- Hashmap/149. Max Points on a Line/149. Max Points on a Line.cpp
+++ Hashmap/149. Max Points on a Line/149. Max Points on a Line.cpp
@@ -1,39 +1,36 @@
-class Solution
-{
+class Solution {
 public:
-    int maxPoints(vector<vector<int>> &points)
+    int maxPoints(vector<vector<int>>& points) 
     {
-        int res = 0;
-        for (auto p : points)
+        int res = 1;
+        for(auto p: points)
         {
-            map<pair<int, int>, int> mp;
-            int v = 0; // for all pts in the vertical(dx == 0)
-            for (auto q : points)
+            int v = 0;
+            map<pair<int, int>, int> cnt; // pair<int, int> must use ordered map
+            // each point has a map, we use the gcd()
+            for(auto q: points)
             {
-                if (p == q)
-                    continue;
-
-                int dx = q[0] - p[0];
-                int dy = q[1] - p[1];
-
-                if (dx == 0)
-                {
-                    v++;
-                    continue;
-                }
-
-                int a = dy / (gcd(dx, dy));
-                int b = dx / (gcd(dx, dy));
-                mp[make_pair(a, b)]++;
+                if(p == q) continue;
+                int dy = p[1] - q[1];
+                int dx = p[0] - q[0];
+                
+                if(dx == 0) 
+                    v += 1;
+                
+                int a = dy/gcd(dy, dx);
+                int b = dx/gcd(dy, dx);
+                cnt[make_pair(a, b)]++;
             }
-
-            res = max(1, res);
-            res = max(res, v + 1);
-            for (auto x : mp)
-            {
-                res = max(x.second + 1, res);
-            }
+            
+            res = max(res, v);
+            for(auto p: cnt)
+                res = max(res, p.second+1);
         }
         return res;
     }
 };
+
+
+// map 里要确保每个斜率的表达方式都是unique的，
+// 那用double的话， 精确度肯定有影响， 最好的办法是用dy/dx的最简分数来标识
+// 那就是说 dy/gcd(dy, dx) , dx/gcd(dy, dx)
